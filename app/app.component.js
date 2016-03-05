7188e8ed4e818d061e6beea3110cca21b6e1ead7
Anton Arzhanik <toshax@tut.by>
2016-03-05 22:03:13 +0300

    Rename

--- app/app.component.js
+++ app/app.component.js
@@ -24,7 +24,7 @@ System.register(['angular2/core'], function(exports_1, context_1) {
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: '<h1>My First Angular 2 App</h1><div>тук тук тукт тук</div>'
+                        template: '<h1>My First Angular 2 App</h1>'
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
